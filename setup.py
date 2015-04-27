9fa01c7ea4d045ed2abd13d3ec5e23e017ffa665
Frank Purcell <fxpurcell@gmail.com>
2015-04-27 16:25:45 -0700

    more basics added to project

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'ott.utils',
+    'waitress',
+]
+
+dev_extras = []
+oracle_extras = ['cx_oracle>=5.1']
+postgresql_extras = ['psycopg2>=2.4.2']
+
+extras_require = dict(
+    dev=dev_extras,
+    #postgresql=postgresql_extras,
+)
+
+setup(
+    name='ott.map',
+    version='0.1.0',
+    description='Open Transit Tools - Map Web Library/Services/App',
+    long_description=README + '\n\n' + CHANGES,
+    keywords='GTFS,GTFS-realtime,GTFSRT',
+    url='http://opentransittools.com',
+    license="Mozilla-derived (http://opentransittools.com)",
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    dependency_links=[
+        'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=requires,
+    test_suite="ott.map.tests",
+    entry_points="""\
+    """,
+)
