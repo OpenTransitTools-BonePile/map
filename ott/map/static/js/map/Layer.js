630e92d5731a163f55856328775ca33ecbcd4bfd
Frank Purcell <fxpurcell@gmail.com>
2015-05-12 11:44:45 -0700

    fix up layer

--- ott/map/static/js/map/Layer.js
+++ ott/map/static/js/map/Layer.js
@@ -1,28 +1,20 @@
 ott.namespace("ott.map");
 
-ott.map.Map = {
+ott.map.Layer = {
 
     map : null,
+    layer : null,
 
     /**
      * @consturctor
      * @param {Object} config
      */
-    initialize : function(config, targetDiv)
+    initialize : function(map)
     {
-        console.log("enter Map() constructor");
+        console.log("enter Layer() constructor");
 
-        // step 1: create the map
-        self.map = new ol.Map({
-            target: targetDiv || 'map',
-            view: new ol.View(config.olMap)
-        });
-
-        var b = new ott.map.BaseLayers(config);
-        map.addLayer(b.getBaseLayersAsGroup());
-        b.makeLayerSwitcher(map, 'Switch Layers');
     },
 
-    CLASS_NAME: "ott.map.Map"
+    CLASS_NAME: "ott.map.Layer"
 };
-ott.map.Map = new ott.Class(ott.map.Map);
+ott.map.Layer = new ott.Class(ott.map.Layer);
