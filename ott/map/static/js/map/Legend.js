1898dac97c7b5f85d02ed6a468c7e82a407dfb74
Frank Purcell <fxpurcell@gmail.com>
2015-05-13 17:06:00 -0700

    legend as class...

--- ott/map/static/js/map/Legend.js
+++ ott/map/static/js/map/Legend.js
@@ -1,38 +1,63 @@
-// Legend functionality
-$(document).ready(function(){
-	$('#legend-wrapper').hide();
+ott.namespace("ott.map");
 
-	$('.legend-btn').click(function(){
-		$('#legend-wrapper').show();
-		$('#legend-btn-wrapper').hide();
-	});
+ott.map.Legend = {
 
-	$('.legend').click(function(){
-		$('#legend-wrapper').hide();
-		$('#legend-btn-wrapper').show();
-	});
-});
+    legend : null,
 
-// Legend appearance
-$(document).ready(function(){
-	$('.cntrl-wrapper').hover(function(){
-		$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
-	},
-	function(){
-		$(this).css('background-color', 'rgba(255, 255, 255, 0.4)');
-	});
+    /**
+     * @consturctor
+     * @param {Object} config
+     */
+    initialize : function(config)
+    {
+        this.showHideHover();
+    },
 
-	$('.legend-btn').hover(function(){
-		$(this).css('background-color', 'rgba(0, 60, 136, 0.7)');
-	},
-	function(){
-		$(this).css('background-color', 'rgba(0, 60, 136, 0.5)');
-	});
+    /**
+     * draw, click and hover routines that manipulate the legend.css and button elements
+     */
+    showHideHover : function()
+    {
+		// Legend functionality
+		$(document).ready(function(){
+			$('#legend-wrapper').hide();
 
-	$('.legend').hover(function(){
-		$(this).css('background-color', 'rgba(255, 255, 255, 0.8)');
-	},
-	function(){
-		$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
-	});
-});
+			$('.legend-btn').click(function(){
+				$('#legend-wrapper').show();
+				$('#legend-btn-wrapper').hide();
+			});
+
+			$('.legend').click(function(){
+				$('#legend-wrapper').hide();
+				$('#legend-btn-wrapper').show();
+			});
+		});
+
+		// Legend appearance
+		$(document).ready(function(){
+			$('.cntrl-wrapper').hover(function(){
+				$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
+			},
+			function(){
+				$(this).css('background-color', 'rgba(255, 255, 255, 0.4)');
+			});
+
+			$('.legend-btn').hover(function(){
+				$(this).css('background-color', 'rgba(0, 60, 136, 0.7)');
+			},
+			function(){
+				$(this).css('background-color', 'rgba(0, 60, 136, 0.5)');
+			});
+
+			$('.legend').hover(function(){
+				$(this).css('background-color', 'rgba(255, 255, 255, 0.8)');
+			},
+			function(){
+				$(this).css('background-color', 'rgba(255, 255, 255, 0.6)');
+			});
+		});
+    },
+
+    CLASS_NAME: "ott.map.Legend"
+};
+ott.map.Legend = new ott.Class(ott.map.Legend);
