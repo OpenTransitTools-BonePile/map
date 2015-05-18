6521e1b11d427c25e5377f4a4ebc9c7167db24e4
Frank Purcell <fxpurcell@gmail.com>
2015-05-18 12:33:27 -0700

    add configurable controls like full screen and rotate

--- ott/map/static/js/config.js
+++ ott/map/static/js/config.js
@@ -72,6 +72,12 @@ if(typeof(ott.config) == "undefined" || ott.config == null)
             }
         ],
 
+
+        // map controls
+        doDragRotate : true,
+        doFullScreen : true,
+
+
         /**
          * map will config the initial map view of the OpenLayers 3.5 map
          * @see: http://openlayers.org/en/v3.5.0/apidoc/ol.View.html
--- ott/map/static/js/map/Map.js
+++ ott/map/static/js/map/Map.js
@@ -13,20 +13,28 @@ ott.map.Map = {
     {
         console.log("enter Map() constructor");
 
-        // step 1: config...
+        // step 1: map div
         this.targetDiv = targetDiv || 'map';
 
-        // step 2: create the map
+        // step 2: map controls
+        var interactions = [];
+        if(config.doDragRotate)
+            interactions.push(new ol.interaction.DragRotateAndZoom());
+
+        var controls = [];
+        if(config.doFullScreen)
+            controls.push(new ol.control.FullScreen());
+
+        // step 3: create the map
         var map = new ol.Map({
             target: document.getElementById(this.targetDiv),
-            interactions: ol.interaction.defaults().extend([
-                new ol.interaction.DragRotateAndZoom()
-            ]),
+            interactions: ol.interaction.defaults().extend(interactions),
+            controls: ol.control.defaults().extend(controls),
             view: new ol.View(config.olMap)
         });
         this.map = map;
 
-        // step 2: add the base layers to the map
+        // step 4: add the base layers to the map
         var b = new ott.map.BaseLayers(config);
         map.addLayer(b.getBaseLayersAsGroup());
         b.makeLayerSwitcher(map, 'Switch Layers');
