6493c3ba600b99432cf330bc88a44630f0e6551f
Frank Purcell <fxpurcell@gmail.com>
2015-08-20 17:04:49 -0700

    add initial leaflet version

---
+++ ott/map/static/js/leaflet/Map.js
@@ -0,0 +1,45 @@
+ott.namespace("ott.map");
+
+ott.map.Map = {
+
+    map : null,
+    targetDiv : null,
+
+    /**
+     * @consturctor
+     * @param {Object} config
+     */
+    initialize : function(config, targetDiv)
+    {
+        console.log("enter Map() constructor");
+
+        // step 1: map div
+        this.targetDiv = targetDiv || 'map';
+
+        // step 2: map controls
+        var interactions = [];
+        if(config.doDragRotate)
+            interactions.push(new ol.interaction.DragRotateAndZoom());
+
+        var controls = [];
+        if(config.doFullScreen)
+            controls.push(new ol.control.FullScreen());
+
+        // step 3: create the map
+        var map = new ol.Map({
+            target: document.getElementById(this.targetDiv),
+            interactions: ol.interaction.defaults().extend(interactions),
+            controls: ol.control.defaults().extend(controls),
+            view: new ol.View(config.olMap)
+        });
+        this.map = map;
+
+        // step 4: add the base layers to the map
+        var b = new ott.map.BaseLayers(config);
+        map.addLayer(b.getBaseLayersAsGroup());
+        b.makeLayerSwitcher(map, 'Switch Layers');
+    },
+
+    CLASS_NAME: "ott.map.Map"
+};
+ott.map.Map = new ott.Class(ott.map.Map);
