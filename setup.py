8f23549621d7d11c021bdf932338a5a4e78e32ab
Frank Purcell <fxpurcell@gmail.com>
2015-09-10 15:08:27 -0700

    more cleanup

--- setup.py
+++ setup.py
@@ -27,6 +27,42 @@ extras_require = dict(
 )
 
 
+setup(
+    name='ott.map',
+    version='0.1.0',
+    description='Open Transit Tools - Map Web Library/Services/App',
+    long_description=README + '\n\n' + CHANGES,
+    keywords='GTFS,GTFS-realtime,GTFSRT',
+    url='http://opentransittools.com',
+    license="Mozilla-derived (http://opentransittools.com)",
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+    dependency_links=[
+        'git+https://github.com/OpenTransitTools/utils.git#egg=ott.utils-0.1.0',
+        ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=requires,
+    test_suite="ott.map.tests",
+    entry_points="""\
+        [paste.app_factory]
+        main = ott.map.pyramid.app:main
+    """,
+)
+
+###
+### file compress routines below...
+###
+
 def filter(name, filters, filter_match, rev=False):
     ''' used to filter certain files from the 'compress' method below
     '''
@@ -71,6 +107,7 @@ def compress(dir, fname='ott.all', ext='js', filters=[], filter_match=True, filt
                 out_file.write(data)
 
     out_file.close()
+
 
 #import pdb; pdb.set_trace()
 compress(dir='ott/map/static/css/', ext='css')
