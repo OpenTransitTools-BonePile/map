ec455f4519a091fdf18b447f7eda8691c25d004b
Frank Purcell <fxpurcell@gmail.com>
2015-09-21 14:11:40 -0700

    get leaf running...

--- ott/map/static/js/leaflet/Map.js
+++ ott/map/static/js/leaflet/Map.js
@@ -26,36 +26,36 @@ ott.leaflet.Map = {
             ;
 
         // step 3: create the map
-		var map = L.map('map');
+        var map = L.map('map');
         this.map = map;
 
         // step 4: add the base layers to the map
-        L.control.layers(baseMaps, overlayMaps).addTo(map);
-		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'mapbox.streets'
-		}).addTo(map);
+        //L.control.layers(baseMaps, overlayMaps).addTo(map);
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(map);
 
-		function onLocationFound(e) {
-			var radius = e.accuracy / 2;
+        function onLocationFound(e) {
+            var radius = e.accuracy / 2;
 
-			L.marker(e.latlng).addTo(map)
-				.bindPopup("You are within " + radius + " meters from this point").openPopup();
+            L.marker(e.latlng).addTo(map)
+                .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-			L.circle(e.latlng, radius).addTo(map);
-		}
+            L.circle(e.latlng, radius).addTo(map);
+        }
 
-		function onLocationError(e) {
-			alert(e.message);
-		}
+        function onLocationError(e) {
+            alert(e.message);
+        }
 
-		map.on('locationfound', onLocationFound);
-		map.on('locationerror', onLocationError);
+        map.on('locationfound', onLocationFound);
+        map.on('locationerror', onLocationError);
 
-		map.locate({setView: true, maxZoom: 16});
+        map.locate({setView: true, maxZoom: 16});
     },
 
     CLASS_NAME: "ott.leaflet.Map"
