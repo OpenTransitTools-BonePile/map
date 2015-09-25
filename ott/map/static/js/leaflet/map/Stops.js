58795df0837eba5ead03cb49be2eb2b3dea4e2bb
Frank Purcell <fxpurcell@gmail.com>
2015-09-24 19:48:11 -0700

    refactor style handling

--- ott/map/static/js/leaflet/map/Stops.js
+++ ott/map/static/js/leaflet/map/Stops.js
@@ -1,98 +1,12 @@
 ott.namespace("ott.leaflet.map");
 
 
-var busStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/bus20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var lightRailStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/rail20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var heavyRailStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/cr20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var streetCarStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/streetcar20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var aerialTramStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/tram20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var subwayStopIcon = L.icon({
-    iconUrl     : '/images/map/stop/subway20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var parkAndRideIcon = L.icon({
-    iconUrl     : '/images/map/stop/pr20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var bikeAndRideIcon = L.icon({
-    iconUrl     : '/images/map/stop/bike20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-var transitCenterRideIcon = L.icon({
-    iconUrl     : '/images/map/stop/tc20.png',
-	iconSize    : [20, 20],
-	iconAnchor  : [10, 10],
-	popupAnchor : [0,  -5]
-});
-
-
-
-var StopIcon20Type = L.Icon.extend({
-    options: {
-        iconUrl: '/images/map/stop/bus20.png',
-        shadowUrl: null,
-        iconSize: new L.Point(20, 20),
-        iconAnchor: new L.Point(10, 10),
-        popupAnchor: new L.Point(0, -5)
-    }
-});
-
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-ott.leaflet.map.StopStyles = {
-};
-
 ott.leaflet.map.Stops = {
 
     map   : null,
     layer : null,
     data  : null,
+    style : null,
     maxZoom     : 14,
     maxFeatures : 500,
 
@@ -105,6 +19,7 @@ ott.leaflet.map.Stops = {
         console.log("enter leaflet Stops() constructor");
 
         // step 1: style
+        this.style = new ott.leaflet.map.TransitIcons();
 
         // step 1: map.Map controller
         this.map = map;
@@ -119,40 +34,8 @@ ott.leaflet.map.Stops = {
 
     baseStyle : function(feature, ll)
     {
-        var retVal = null;
-        switch (feature.properties.type)
-        {
-            case 1:
-                retVal = L.marker(ll, {icon:busStopIcon});
-                break;
-            case 2:
-                retVal = L.marker(ll, {icon:streetCarStopIcon});
-                break;
-            case 3:
-                retVal = L.marker(ll, {icon:aerialTramStopIcon});
-                break;
-            case 4:
-                retVal = L.marker(ll, {icon:heavyRailStopIcon});
-                break;
-            case 5:
-                retVal = L.marker(ll, {icon:lightRailStopIcon});
-                break;
-            case 10:
-                retVal = L.marker(ll, {icon:parkAndRideIcon});
-                break;
-            case 14:
-                retVal = L.marker(ll, {icon:transitCenterRideIcon});
-                break;
-            case 17:
-                retVal = L.marker(ll, {icon:bikeAndRideIcon});
-                break;
-            default:
-                retVal = L.circleMarker(ll, geojsonMarkerOptions);
-                break;
-        }
-
-        // TODO: we really have to add to layer here????  future versions of code
-        retVal.addTo(this.layer);
+        var retVal = this.style.getStyleByTypeId(feature.properties.type);
+        retVal.addTo(this.layer); // TODO: we really have to add to layer here????  future versions of code
         return retVal;
     },
 
---
+++ ott/map/static/js/leaflet/map/TransitIcons.js
@@ -0,0 +1,121 @@
+ott.namespace("ott.leaflet.map");
+
+ott.leaflet.map.TransitIcons = {
+
+    busStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/bus20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    lightRailStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/rail20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    heavyRailStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/cr20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    streetCarStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/streetcar20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    aerialTramStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/tram20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    subwayStopIcon : L.icon({
+        iconUrl     : '/images/map/stop/subway20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    parkAndRideIcon : L.icon({
+        iconUrl     : '/images/map/stop/pr20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    bikeAndRideIcon : L.icon({
+        iconUrl     : '/images/map/stop/bike20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    transitCenterRideIcon : L.icon({
+        iconUrl     : '/images/map/stop/tc20.png',
+        iconSize    : [20, 20],
+        iconAnchor  : [10, 10],
+        popupAnchor : [0,  -5]
+    }),
+
+    geojsonMarkerOptions : {
+        radius: 6,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    },
+
+    /**
+     * these are how trimet orders our styles
+     */
+    triMetPOIStyles : function(id, ll)
+    {
+        var retVal = null;
+
+        switch(id)
+        {
+            case 1:
+                retVal = L.marker(ll, {icon: this.busStopIcon});
+                break;
+            case 2:
+                retVal = L.marker(ll, {icon: this.streetCarStopIcon});
+                break;
+            case 3:
+                retVal = L.marker(ll, {icon: this.aerialTramStopIcon});
+                break;
+            case 4:
+                retVal = L.marker(ll, {icon: this.heavyRailStopIcon});
+                break;
+            case 5:
+                retVal = L.marker(ll, {icon: this.lightRailStopIcon});
+                break;
+            case 10:
+                retVal = L.marker(ll, {icon: this.parkAndRideIcon});
+                break;
+            case 14:
+                retVal = L.marker(ll, {icon: this.transitCenterRideIcon});
+                break;
+            case 17:
+                retVal = L.marker(ll, {icon: this.bikeAndRideIcon}),
+                break;
+            default:
+                retVal = L.circleMarker(ll, geojsonMarkerOptions);
+                break;
+        };
+        return retVal;
+    },
+
+    getStyleByTypeId : triMetPOIStyles,
+
+    CLASS_NAME: "ott.leaflet.map.TransitIcons"
+};
+ott.leaflet.map.TransitIcons = new ott.Class(ott.leaflet.map.TransitIcons);
