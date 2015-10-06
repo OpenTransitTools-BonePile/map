b44992a478e2cbbdb24cb42e3a07b0e2b56a70a5
Frank Purcell <fxpurcell@gmail.com>
2015-10-06 14:14:21 -0700

    move of trip planner stuff

---
+++ ott/map/static/js/planner/Controller.js
@@ -0,0 +1,16 @@
+ott.namespace("ott.planner");
+
+
+ott.planner.Controller = {
+    /**
+     * @consturctor
+     * @param {Object} config
+     */
+    initialize : function(config, map)
+    {
+        console.log("enter planner.Controller() constructor");
+    },
+
+    CLASS_NAME: "ott.planner.Controller"
+};
+ott.planner.Controller = new ott.Class(ott.planner.Controller);
