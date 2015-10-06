989f362d16e32c9bea3806ad8ed372973cd759ba
Frank Purcell <fxpurcell@gmail.com>
2015-10-06 15:41:30 -0700

    initial add of Params

--- ott/map/static/js/planner/Controller.js
+++ ott/map/static/js/planner/Controller.js
@@ -2,13 +2,28 @@ ott.namespace("ott.planner");
 
 
 ott.planner.Controller = {
+
+    config : null,
+    params : null,
+
     /**
      * @consturctor
      * @param {Object} config
      */
-    initialize : function(config, map)
+    initialize : function(config, params)
     {
         console.log("enter planner.Controller() constructor");
+        this.config = config;
+        if(params)
+        {
+            this.callPlannerWs(params);
+        }
+    },
+
+    callPlannerWs : function(params)
+    {
+        this.params = params;
+
     },
 
     CLASS_NAME: "ott.planner.Controller"
---
+++ ott/map/static/js/planner/Params.js
@@ -0,0 +1,24 @@
+ott.namespace("ott.planner");
+
+
+ott.planner.Params = {
+
+    from : null,
+    to   : null,
+    maxWalk : null,
+
+
+    /** @consturctor */
+    initialize : function()
+    {
+    },
+
+    makePlannerWsUrl : function(config)
+    {
+        var retVal;
+        return retVal;
+    },
+
+    CLASS_NAME: "ott.planner.Params"
+};
+ott.planner.Params = new ott.Class(ott.planner.Params);
