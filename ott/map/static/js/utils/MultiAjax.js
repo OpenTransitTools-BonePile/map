e57aa502a416f89cc2a6623a11a327c4d81c81e9
Frank Purcell <fxpurcell@gmail.com>
2015-12-07 23:50:13 -0800

    multi ajax query is kinda running

--- ott/map/static/js/utils/MultiAjax.js
+++ ott/map/static/js/utils/MultiAjax.js
@@ -2,11 +2,76 @@ ott.namespace("ott.utils");
 
 ott.utils.MultiAjax = {
 
-    callz : function(urls)
+    urls : null,
+    data : [],
+    isBusy : false,
+
+    initialize : function(urls)
     {
+        this.urls = urls;
+
+        // prep the data structure with content / null for ajax return
+        for(var i = 0; i < urls.length; i++)
+        {
+            var u = urls[i];
+            var n = ott.utils.StringUtils.serviceNameFromURL(u);
+            var d = {'url':u, 'name':n, 'data':null}
+            this.data.push(d);
+        }
+
+        this.callUrls()
     },
 
-    callUrls : function(urls)
+    callUrls : function()
+    {
+        var this_  = this;
+
+        var index = 0;
+        var error = false;
+        var execute_queue = function(index)
+        {
+            this_.isBusy = true;
+            $.ajax({
+                url: this_.data[index].url,
+                success: function(resp)
+                {
+                    this_.data[index].data = resp;
+
+                    index++;
+                    error = false;
+                    if(this_.data[index] != undefined)
+                    {
+                        execute_queue(index);
+                        this_.isBusy = false;
+                    }
+                },
+                error: function(resp)
+                {
+                    if(!error)
+                    {
+                        error = true;
+                        console.log("error from url " + this_.data[index].url + "; retrying query");
+                        execute_queue(index);
+                        this_.isBusy = false;
+                    }
+                    else
+                    {
+                        index++;
+                        error = false;
+                        if(this_.data[index] != undefined)
+                        {
+                            execute_queue(index);
+                            this_.isBusy = false;
+                        }
+                    }
+                }
+            });
+        };
+        execute_queue(index);
+    },
+
+/*
+    XcallUrls : function(urls)
     {
        var retVal = [];
 
@@ -29,11 +94,7 @@ ott.utils.MultiAjax = {
         };
 
         $.when(
-
-            for(var i = 0; i < urls; i++)
-            {
-                call(urls[i]);
-            }
+            call(urls[i]);
         ).done(function()
         {
             console.log('hhhh');
@@ -42,7 +103,7 @@ ott.utils.MultiAjax = {
                     console.log(urls[i]);
         });
 
-/*
+
             call('/js/ott.js')
             ,
             call('/js/ott.leaflet.js')
@@ -50,9 +111,10 @@ ott.utils.MultiAjax = {
 for(var i = 0; i < urls; i++)
             {
                 call(urls[i]);
-            }*/
+            }
     },
-
-    CLASS_NAME: "trimet.utils.MultiAjax"
+*/
+    CLASS_NAME: "ott.utils.MultiAjax"
 }
+ott.utils.MultiAjax = new ott.Class(ott.utils.MultiAjax);
 
