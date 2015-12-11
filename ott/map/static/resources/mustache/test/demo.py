45ff93cb082b4ad4faa9112a459104bd3a13e3ca
Frank Purcell <fxpurcell@gmail.com>
2015-12-11 02:49:14 -0800

    python mustache processor now running

--- ott/map/static/resources/mustache/test/demo.py
+++ ott/map/static/resources/mustache/test/demo.py
@@ -1,5 +1,14 @@
 import pystache
+import simplejson as json
 
+with open('demo.json') as data_file:
+    data = json.load(data_file)
+
+fh = open('demo.mustache')
+template = fh.read()
+fh.close()
 
 renderer = pystache.Renderer()
-print renderer.render(demo)
+print renderer.render(template, data)
+#print template, data
+
