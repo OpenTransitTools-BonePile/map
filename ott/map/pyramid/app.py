41d562e8a34e30ee27d7a67b693884e9796d48ef
Frank Purcell <fxpurcell@gmail.com>
2016-01-05 17:32:28 -0800

    pyramid fixes (really bad bug)

--- ott/map/pyramid/app.py
+++ ott/map/pyramid/app.py
@@ -23,7 +23,6 @@ def do_static_config(config, path='ott.map', prefixes=['', 'm/', 'ws/'], cache_a
     ''' config the static folders
     '''
     for p in prefixes:
-        config.add_static_view(p + '',          path + ':static',           cache_max_age=cache_age)
         config.add_static_view(p + 'resources', path + ':static/resources', cache_max_age=cache_age)
         config.add_static_view(p + 'js',        path + ':static/js',        cache_max_age=cache_age)
         config.add_static_view(p + 'css',       path + ':static/css',       cache_max_age=cache_age)
