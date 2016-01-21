4f791a6d60f96644f82038cd86ffbd97b63ff2aa
Frank Purcell <fxpurcell@gmail.com>
2016-01-20 18:06:57 -0800

    clean up ... bye routes.js and track down bogus global vars

--- ott/map/static/js/ott.js
+++ ott/map/static/js/ott.js
@@ -7,8 +7,7 @@ if(window.console == undefined || window.console.log == undefined)
     var names = [
                  "log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
                  "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"
-        ];
-
+    ];
     window.console = {};
     for (var i = 0; i < names.length; ++i)
         window.console[names[i]] = function(){};
