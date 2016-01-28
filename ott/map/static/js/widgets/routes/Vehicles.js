0b4a5c24e51bf9ea7e9f1616a7eadbdd17ba7836
Frank Purcell <fxpurcell@gmail.com>
2016-01-28 11:17:24 -0800

    a bit of route sync work and older home stuff

--- ott/map/static/js/widgets/routes/Vehicles.js
+++ ott/map/static/js/widgets/routes/Vehicles.js
@@ -1,22 +1,34 @@
 ott.namespace("ott.widgets.routes");
 
+
 ott.widgets.routes.Vehicles = {
 
     map : null,
-    routes : null,
+    url : null,
 
     /**
      * @consturctor
      */
-    initialize : function(map, targetDiv, url)
+    initialize : function(map, url, buttonDivName, timeSliderDivName)
     {
         ott.widgets.Widget.prototype.initialize.apply(this, arguments);
+        // route, buttonDivName, timeSliderDivName;
     },
 
     /**
      * real-time vehicles for selected routes
      */
     showVehicles : function(selectedRoutes)
+    {
+        for(var i in selectedRoutes)
+        {
+        }
+    },
+
+    /**
+     * time slider
+     */
+    timeSlider : function(selectedRoutes)
     {
         for(var i in selectedRoutes)
         {
