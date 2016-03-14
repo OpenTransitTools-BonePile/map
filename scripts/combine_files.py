42033daeebcb64f73bde8b84d6ae9b90005858ec
Frank Purcell <fxpurcell@gmail.com>
2016-03-14 00:17:38 -0700

    more template fix

--- scripts/combine_files.py
+++ scripts/combine_files.py
@@ -98,28 +98,27 @@ def close_templates_output(out_name, out_file, last_line="\n    'CLASS_NAME' : '
     out_file.write(ll)
     out_file.close()
 
+def combine_files():
+    combine(dir='ott/map/static/css/', ext='css', fname='ott.min')
+    combine(dir='ott/map/static/js/',  ext='js',  fname='ott.min', filters=['tests', 'openlayers', 'config'], filter_match=False, filter_dirs=True)
+    combine(dir='ott/map/static/resources/leaflet/', ext='js',  fname='ott.leaflet', filters=['leaflet-src'], filter_match=False, filter_dirs=True)
+    combine(dir='ott/map/static/resources/leaflet/', ext='css', fname='ott.leaflet', filters=['leaflet-src'], filter_match=False, filter_dirs=True)
+    #combine(dir='ott/map/static/js/',  ext='js', fname='ott.openlayers', filters=['leaflet', 'config'],     filter_match=False, filter_dirs=True)
+
+    # planner
+    dir='ott/map/static/js/widgets/planner/templates/'
+    out_name, out_file = open_templates_output(dir, "ott.planner.templates")
+    templates_to_js(dir, out_name, out_file)
+    close_templates_output(out_name, out_file)
+
+    # test
+    dir='ott/map/static/resources/mustache/test/'
+    out_name, out_file = open_templates_output(dir, "test.templates")
+    templates_to_js(dir, out_name, out_file)
+    close_templates_output(out_name, out_file)
+
 def main(argv=None):
-    #import pdb; pdb.set_trace()
-    all = (argv == None or len(argv) == 1 or "all" in argv)
-    if all or "combo" in argv or "c" in argv:
-        combine(dir='ott/map/static/css/', ext='css', fname='ott.min')
-        combine(dir='ott/map/static/js/',  ext='js',  fname='ott.min', filters=['tests', 'openlayers', 'config'], filter_match=False, filter_dirs=True)
-        combine(dir='ott/map/static/resources/leaflet/', ext='js',  fname='ott.leaflet', filters=['leaflet-src'], filter_match=False, filter_dirs=True)
-        combine(dir='ott/map/static/resources/leaflet/', ext='css', fname='ott.leaflet', filters=['leaflet-src'], filter_match=False, filter_dirs=True)
-        #combine(dir='ott/map/static/js/',  ext='js', fname='ott.openlayers', filters=['leaflet', 'config'],     filter_match=False, filter_dirs=True)
-
-    if all or "templates" in argv or "t" in argv:
-        # planner
-        dir='ott/map/static/js/widgets/planner/templates/'
-        out_name, out_file = open_templates_output(dir, "ott.planner.templates")
-        templates_to_js(dir, out_name, out_file)
-        close_templates_output(out_name, out_file)
-
-        # test
-        dir='ott/map/static/resources/mustache/test/'
-        out_name, out_file = open_templates_output(dir, "test.templates")
-        templates_to_js(dir, out_name, out_file)
-        close_templates_output(out_name, out_file)
+    combine_files()
 
 if __name__ == "__main__":
     main(sys.argv)
